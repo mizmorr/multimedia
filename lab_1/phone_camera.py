c59f91537191b4439dccfec8c9fdf6163700e89f
mizmorr <mizmorrr@gmail.com>
2023-09-12 23:15:10 +0300

    phone camera reading done

---
+++ lab_1/phone_camera.py
@@ -0,0 +1,18 @@
+import requests
+import cv2
+import numpy as np
+
+url = "http://192.168.0.140:8080/shot.jpg"
+
+
+while True:
+    img_resp = requests.get(url)
+    img_arr = np.array(bytearray(img_resp.content), dtype=np.uint8)
+    img = cv2.imdecode(img_arr, -1)
+    img = cv2.resize(img, (720,480))
+    cv2.imshow("Android_cam", img)
+    cv2.moveWindow("Android_cam",500,0)
+    if cv2.waitKey(1) == 27:
+        break
+
+cv2.destroyAllWindows()
