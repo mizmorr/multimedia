d2c43d27170b5bd773cbc49017d05eb70c732587
mizmorr <mizmorrr@gmail.com>
2023-09-19 11:16:17 +0300

    camera to hsv done

---
+++ lab_2/camera_into_hsv.py
@@ -0,0 +1,18 @@
+import numpy as np
+import cv2 as cv
+
+cap = cv.VideoCapture(0)
+if not cap.isOpened():
+ print("Cannot open camera")
+ exit()
+while True:
+ ret, frame = cap.read()
+ hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+ cv.imshow('rgb', frame)
+ cv.moveWindow("hsv",700,55)
+ cv.imshow('hsv',hsv)
+ if cv.waitKey(1) == ord('q'):
+    break
+
+cap.release()
+cv.destroyAllWindows()
